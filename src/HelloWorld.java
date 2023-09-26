7582e1506e51be7efeac7f1bf4df2f73129367ff
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-09-26 14:50:50 -0500

    created first java file

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+//Java is Object Oriented
+//organizes software design around data (objects) rather than functions and logic
+//objects is a data field that has unique attributes and behavior
+//OOP focuses on the objects that we want to manipulate rather than the logic required to manipulate them
+//OOP is best suited for large, complex programs
+
+//Classes - data types that act as the blueprint for individual objects, attributes, and methods
+
+//Objects - instances of a class created with specifically defined data
+
+
