d8dcd253a2749fe7f143de4427390f0e66b6fd17
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-09-27 15:44:15 -0500

    completed syntax-types-and-variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,49 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 16;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is my string.";
+        System.out.println(myString);
+
+        float myNumber = 3.14f;
+
+
+        //L tells the compiler that we have a long number literal
+
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x = x + 5;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        y *= x;
+
+        int x = 10;
+        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//        x /= y;
+        y -= x;
+
+
     }
 }
 //Java is Object Oriented
@@ -8,9 +51,14 @@ public class HelloWorld {
 //objects is a data field that has unique attributes and behavior
 //OOP focuses on the objects that we want to manipulate rather than the logic required to manipulate them
 //OOP is best suited for large, complex programs
-
 //Classes - data types that act as the blueprint for individual objects, attributes, and methods
-
 //Objects - instances of a class created with specifically defined data
+//Use camelCase for variable and method names
+//Use PascalCase for class names
+//In Java:
+//Instead of const we use final - final variables are in SCREAMING_SNAKE_CASE
+
+
+
 
 
