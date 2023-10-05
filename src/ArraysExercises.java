9baf2dedaef256c60c26acfe318ac10d5bd8be11
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-10-04 19:23:07 -0500

    completed first part of arrays exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -2,8 +2,6 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
-
-
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
@@ -17,9 +15,19 @@ public class ArraysExercises {
             System.out.println(person.name);
         }
 
-
+        addPerson(people, new Person("Cali", 6, "female", "house cat"));
 
     }
 
+    public static Person[] addPerson(Person[] people, Person person) {
+        Person[] peopleCopy = Arrays.copyOf(people, people.length + 1);
+        peopleCopy[peopleCopy.length - 1] = person;
+        System.out.println(peopleCopy.length);
+        for (Person persons : peopleCopy) {
+
+            System.out.println(persons.name);
+        }
+        return peopleCopy;
+    }
 
 }
